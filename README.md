191d9ceb6fd831cb20870b90bff27da32cac2b7c
Ryan Boye <rpboye@gmail.com>
2016-04-19 21:53:20 -0700

    Day 1! Output the body of the requested URL in C#

--- README.md
+++ README.md
@@ -16,4 +16,23 @@ Ideas for upcoming days (some may span multiple):
   - Write a maze generator
 - Write a document crawler for Windows 10 in C#
   - write a file search for Windows 10 in C#
- 
+- Exercises from the data science / machine learning course for python [DAT8 Course repository](https://github.com/justmarkham/DAT8)
+  - hold because there are a bunch of things in this course
+  - Data plotting
+  - Regression
+  - Python command line stuff
+- Experiment with [PT JS](https://github.com/williamngan/pt)
+- Natural language processing
+- Write something using docker
+- Set something up on AWS
+- Expand on the CloudFlare Dash
+- Try ember
+- Try React
+- Past to future self idea tracking / note taking app
+- Fun money
+  - Transaction parser, what are your monthly costs?
+  - Apply some data science, what can you extract?
+- Enter a kaggle competition
+- Write a tower of hanoi solver
+- Write implementations of [basic algorithms](https://en.wikipedia.org/wiki/List_of_algorithms)
+  
---
+++ Web Crawler/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace Web_Crawler
+{
+    class Program
+    {
+        static void Main()
+        {
+            var target = Console.ReadLine();
+
+            if (target.IndexOf("http://") != -1)
+            {
+                RequestSite(target);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Please enter a valid URL");
+                Console.ResetColor();
+
+                Main();
+            }
+        }
+
+        static void RequestSite(string target)
+        {
+            HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create(target);
+            var response = (HttpWebResponse)myReq.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            response.Close();
+
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Scanning: " + target);
+            Console.ResetColor();
+
+            Console.WriteLine(responseString);
+            Main();
+        }
+    }
+}
+
+
+
