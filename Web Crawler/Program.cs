b90a1dce27757d691f6cb73a3bf5cd538d411dfd
Ryan Boye <rpboye@gmail.com>
2016-04-20 22:25:09 -0700

    Day 2 - Trying to get links out of the body..

--- Web Crawler/Program.cs
+++ Web Crawler/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.IO;
-using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
 
 namespace Web_Crawler
 {
@@ -26,18 +26,44 @@ namespace Web_Crawler
 
         static void RequestSite(string target)
         {
-            HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create(target);
-            var response = (HttpWebResponse)myReq.GetResponse();
-            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
-            response.Close();
+            var client = new HttpClient();
+            var response = client.GetAsync(target).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                // by calling .Result you are performing a synchronous call
+                var responseContent = response.Content;
+
+                // by calling .Result you are synchronously reading the result
+                string responseString = responseContent.ReadAsStringAsync().Result;
+
+                getLinks(responseString);
+            }
 
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Scanning: " + target);
             Console.ResetColor();
-
-            Console.WriteLine(responseString);
+          
             Main();
+        }
+
+        static void getLinks(string body)
+        {
+            
+            Regex rx= new Regex("@(?=href=\"([^\"]*)\")[^>]*>([^<]*)<\\/a>",
+                RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            MatchCollection matches = rx.Matches(body);
+
+            foreach (Match match in matches)
+            {
+                GroupCollection groups = match.Groups;
+                Console.WriteLine("'{0}' repeated at positions {1} and {2}",
+                                  groups["word"].Value,
+                                  groups[0].Index,
+                                  groups[1].Index);
+            }
         }
     }
 }
