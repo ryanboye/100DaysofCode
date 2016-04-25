5b1caa3eec981839dd70546239cd2710333629b4
Ryan Boye <rpboye@gmail.com>
2016-04-24 22:35:19 -0700

    Day 6 - Not much progress

--- Web Crawler/Program.cs
+++ Web Crawler/Program.cs
@@ -7,12 +7,17 @@ namespace Web_Crawler
 {
     class Program
     {
-        static Dictionary<string, string> targets =
-            new Dictionary<string, string>();
+        static Dictionary<string, string> targets = new Dictionary<string, string>();
+        static Dictionary<string, string> history = new Dictionary<string, string>();
+        private static readonly Regex slash_re = new Regex("/\\/{ 2,}",
+                RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace);
+        private static readonly Regex re = new Regex("<a.*?href=\"(.*?)\"",
+                RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace);
 
-        static void Main()
+    static void Main()
         {
-            var target = Console.ReadLine();
+            // var target = Console.ReadLine();
+            var target = "http://leftronic.com";
 
             if (target.IndexOf("http://") != -1)
             {
@@ -33,6 +38,11 @@ namespace Web_Crawler
             var client = new HttpClient();
             var response = client.GetAsync(target).Result;
 
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Scanning: " + target);
+            Console.ResetColor();
+
             if (response.IsSuccessStatusCode)
             {
                 // by calling .Result you are performing a synchronous call
@@ -44,36 +54,61 @@ namespace Web_Crawler
                 getLinks(responseString, target);
             }
 
-            Console.BackgroundColor = ConsoleColor.Blue;
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("Scanning: " + target);
-            Console.ResetColor();
-          
-            Main();
+            //Main();
+            Console.ReadLine();
         }
 
         static void getLinks(string body, string target)
         {
-            
-            Regex rx= new Regex("(?=href=\"([^ \"]*)\")[^>]*>[^<]*<\\/a>",
-                RegexOptions.IgnoreCase | RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace);
-
-            MatchCollection matches = rx.Matches(body);
+            MatchCollection matches = re.Matches(body);
 
             foreach (Match match in matches)
             {
-                string newTarget = target + match.Groups[1].Value;
-               
-                try
+                string current_match = match.Groups[1].Value;
+                string newTarget = target + current_match;
+                Console.WriteLine("Match" + current_match);
+
+                char first_letter = current_match[0];
+
+                //Internal site match
+                if (first_letter == '/')
                 {
-                    targets.Add(newTarget, newTarget);  //yea I'm sorry
-                    RequestSite(newTarget);
+                    var newTarget = 'http://' + (target + match).replace('http://', '').replace(slash_re, '/');
+                    // console.log((target + match).green);
+                    // console.log(history);
+                    if (history[newTarget] == null)
+                    {
+                        console.log('NEW: '.green + (newTarget));
+                    }
+                    else if (history[newTarget] == 1)
+                    {
+                        console.log('EXISTS: '.yellow + (newTarget));
+                    }
+                    if (history[newTarget] == null)
+                    {
+                        getSite(newTarget);
+                    }
                 }
-                catch (ArgumentException)
+
+                // external site yeeee
+                if (match.indexOf('http://') == '0')
                 {
-                    Console.WriteLine("An element with Key = {0} already exists.", newTarget);
+                    var newTarget = match;
+                    if (history[newTarget] == null)
+                    {
+                        console.log('NEW: '.green + (newTarget));
+                    }
+                    else if (history[newTarget] == 1)
+                    {
+                        console.log('EXISTS: '.yellow + (newTarget));
+                    }
+                    if (history[newTarget] == null)
+                    {
+                        getSite(newTarget);
+                    }
                 }
             }
+            Console.ReadLine();
         }
     }
 }
