474d927cb07c16190b301d0b476814de58aa6f85
Ryan Boye <rpboye@gmail.com>
2016-04-30 07:52:49 -0700

    blah async

---
+++ Web Crawler/CrawlLogger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace Web_Crawler
+{
+    public class CrawlLogger
+    {
+        private BlockingCollection<string> _collection;
+
+        public CrawlLogger(string file_path)
+        {
+            _collection = new BlockingCollection<string>(10000);
+            var t = Task.Factory.StartNew(() =>
+            {
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(file_path))
+                {
+                    while (true)
+                    {
+                        var text = _collection.Take();
+                        file.WriteLine(text);
+                        file.Flush();
+                    }
+                }
+
+            }, TaskCreationOptions.LongRunning);
+
+        }
+
+        public void AddLog(string logItem)
+        {
+            _collection.Add(logItem);         
+        }
+    }
+}
