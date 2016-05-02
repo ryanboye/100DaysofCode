70a0e2824faa313fe780c0f7412ad537a391862c
Ryan Boye <rpboye@gmail.com>
2016-05-01 19:33:35 -0700

    Refactor and updating console

---
+++ Web Crawler/CrawlerEngine.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Web_Crawler
+{
+    class CrawlerEngine
+    {
+        public static Dictionary<string, string> targets = new Dictionary<string, string>();
+        public static Dictionary<string, string> history = new Dictionary<string, string>();
+
+        private static CrawlLogger crawllogger;
+        public static Stack<string> _urls = new Stack<string>();
+        public static int taskCount;
+        private static List<Task<string>> _tasks = new List<Task<string>>();
+        private int _maxConcurrency;
+
+        private static Stack<string> messageQueue = new Stack<string>();
+        public Stack<string> MessageQueue
+        {
+            get
+            {
+                return messageQueue;
+            }
+        }
+        private static string _lastCrawled;
+        public string last_crawled
+        {
+            get
+            {
+                return _lastCrawled;
+            }
+        }
+
+        public int MaxConcurrency
+        {
+            get
+            {
+                return _maxConcurrency;
+            }
+            set
+            {
+                ServicePointManager.DefaultConnectionLimit = value + 100;
+                _maxConcurrency = value;
+            }
+        }
+        public int TaskCount { get; set; }
+        
+        public CrawlerEngine(int maxConcurrency = 20)
+        {
+            MaxConcurrency = maxConcurrency;
+            crawllogger = new CrawlLogger("crawlerlogger-log.txt");
+            taskCount = 0;
+        }
+
+        public async Task<int> crawl(string target)
+        {
+
+            _urls.Push(target);
+            while (_urls.Count > 0 || _tasks.Count > 0)
+            {
+                while (taskCount < MaxConcurrency && _urls.Count > 0)
+                {
+                    _tasks.Add(RequestSite(_urls.Pop()));
+                }
+
+                Task<string> t = await Task.WhenAny(_tasks);
+
+                var completed = _tasks.Where(x => x.Status == TaskStatus.RanToCompletion).ToList();
+                foreach (var task in completed)
+                {
+                    getLinks(task.Result, target);
+                    _tasks.Remove(task);
+                }
+            }
+
+            return history.Count();
+        }
+
+
+        static async Task<string> RequestSite(string target)
+        {
+            string responseString = "";
+            try
+            {
+                history.Add(target, "true");
+                HttpClient client = new HttpClient();
+                Task<HttpResponseMessage> response_contents = client.GetAsync(target);
+
+                // Console.BackgroundColor = ConsoleColor.Blue;
+                // Console.ForegroundColor = ConsoleColor.White;
+                // Console.ResetColor();
+                messageQueue.Push("Scanning:" + target + "\n");
+                taskCount++;
+                HttpResponseMessage response = await response_contents;
+                crawllogger.AddLog(target);
+                taskCount--;
+
+                try
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var responseContent = response.Content;
+                        responseString = responseContent.ReadAsStringAsync().Result;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    responseString = "";
+                }
+            }
+            catch (ArgumentException)
+            {
+                messageQueue.Push("An element with Key = " + target + " already exists.");
+                responseString = "";
+            }
+
+            getLinks(responseString, target);
+            return responseString;
+
+        }
+
+        static void getLinks(string body, string target)
+        {
+            _lastCrawled = target;
+            Regex slash_re = new Regex("/{2,}",
+                RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace);
+            Regex re = new Regex("<a.*?href=\"(.*?)\"",
+                    RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace);
+
+            MatchCollection matches = re.Matches(body);
+
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                {
+                    var test = matches;
+                    string current_match = "";
+                    current_match = match.Groups[1].Value;
+
+                    string newTarget = target + current_match;
+                    //messageQueue.Push(match.Groups);
+
+
+
+                    // Internal site match
+                    if (current_match.FirstOrDefault() == '/')
+                    {
+                        newTarget = "http://" + slash_re.Replace((target + current_match).Replace("http://", ""), "/");
+
+                        // Console.ForegroundColor = ConsoleColor.Green;
+                        //messageQueue.Push(newTarget);
+                        // Console.ResetColor();
+
+                        if (!history.ContainsKey(newTarget))
+                        {
+                            // Console.ForegroundColor = ConsoleColor.Green;
+                            //messageQueue.Push("NEW: " + (newTarget));
+                            // Console.ResetColor();
+                            _urls.Push(newTarget);
+                        }
+                        else if (history.ContainsKey(newTarget))
+                        {
+                            // Console.ForegroundColor = ConsoleColor.Yellow;
+                            //messageQueue.Push("EXISTS: " + (newTarget));
+                            // Console.ResetColor();
+                        }
+
+                    }
+
+                    //external site
+
+                    if (current_match.IndexOf("http") == 0 || current_match.IndexOf("https") == 0)
+                    {
+                        newTarget = current_match;
+                        if (!history.ContainsKey(newTarget))
+                        {
+                            // Console.ForegroundColor = ConsoleColor.Green;
+                            //messageQueue.Push("NEW EXTERNAL: " + (newTarget));
+                            // Console.ResetColor();
+                            _urls.Push(newTarget);
+                        }
+                        else if (history.ContainsKey(newTarget))
+                        {
+                            // Console.ForegroundColor = ConsoleColor.Yellow;
+                            //messageQueue.Push("EXISTING EXTERNAL: " + (newTarget));
+                            // Console.ResetColor();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
--- Web Crawler/Program.cs
+++ Web Crawler/Program.cs
@@ -1,202 +1,70 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Web_Crawler
 {
-    class Program
+    public class Program
     {
-        static Dictionary<string, string> targets = new Dictionary<string, string>();
-        static Dictionary<string, string> history = new Dictionary<string, string>();
-        private static readonly Regex slash_re = new Regex("/{2,}",
-                RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace);
-        private static readonly Regex re = new Regex("<a.*?href=\"(.*?)\"",
-                RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace);
-        private static CrawlLogger crawllogger;
+        private static CrawlerEngine crawler_engine = new CrawlerEngine(20);
 
-        private static Stack<string> _urls = new Stack<string>();
-        private static int _maxConcurrency = 200;
-        private static int taskCount;
-        private static List<Task<string>> _tasks = new List<Task<string>>();
-
-        static void Main()
+        static void Main(string[] args)
         {
-            var target = "http://leftronic.com";
-            taskCount = 0;
-            crawllogger = new CrawlLogger("crawlerlogger-log.txt");
-            
-
-            if (target.IndexOf("http://") != -1)
-            {
-                crawl(target);
-                Console.ReadLine();
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Please enter a valid URL");
-                Console.ResetColor();
-
-                Main();
-            }
+            var target = args.Length > 0 ? args[0] : "http://leftronic.com";
+            var mainTasks = new List<Task>();
+            var crawlTask = crawler_engine.crawl(target);
+            var inputTask = InputAsync();
+            var consoleTask = ConsoleAsync();
+            mainTasks.Add(crawlTask);
+            mainTasks.Add(inputTask);
+            mainTasks.Add(consoleTask);
+            Task.WaitAll(mainTasks.ToArray());
         }
 
-        static async void crawl(string target)
+        private static async Task InputAsync()
         {
-            
-            _urls.Push(target);
-            while (_urls.Count > 0)
+            await Task.Factory.StartNew(() =>
             {
-                while (taskCount < _maxConcurrency && _urls.Count > 0)
+
+                var key = Console.ReadKey().Key;
+                while (key != ConsoleKey.Q)
                 {
-                    _tasks.Add(RequestSite(_urls.Pop()));
+                    if (key == ConsoleKey.RightArrow)
+                    {
+                        crawler_engine.MaxConcurrency += 20;
+                    }
+                    else if (key == ConsoleKey.LeftArrow)
+                    {
+                        crawler_engine.MaxConcurrency += 20;
+                    }
+                    key = Console.ReadKey().Key;
                 }
-
-                Task<string> t = await Task.WhenAny(_tasks);
-                string result = await t;
-                getLinks(result, target);                
-            }
-
-            //var key = Console.ReadKey().Key;
-            //while (key != ConsoleKey.Q)
-            //{
-            //    Console.WriteLine("Main loop");
-            //    while (_urls.Count > 0)
-            //    {
-            //        while (taskCount < _maxConcurrency && _urls.Count > 0)
-            //        {
-            //            _tasks.Add(RequestSite(_urls.Pop()));
-            //        }
-
-            //        Task<string> t = await Task.WhenAny(_tasks);
-            //        string result = await t;
-            //        getLinks(result);
-            //        key = Console.ReadKey().Key;
-            //    }
-            //}
-            //Environment.Exit(0);
+                Environment.Exit(0);
+            });
         }
 
-
-        static async Task<string> RequestSite(string target)
+        private static async Task ConsoleAsync()
         {
-            string responseString = "";
-            try
+            Console.Clear();
+            while(true)
             {
-                history.Add(target, "true");
-                HttpClient client = new HttpClient();
-                Task<HttpResponseMessage> response_contents = client.GetAsync(target);
+                //var lastCrawled = crawler_engine.last_crawled.Length > 50 ? crawler_engine.last_crawled : crawler_engine.last_crawled.Substring(0, 49);
+                var lastCrawled = crawler_engine.last_crawled;
 
-                Console.BackgroundColor = ConsoleColor.Blue;
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.Write("Scanning:");
-                Console.ResetColor();
-                Console.Write(" " + target + "\n");
-                taskCount++;
-                HttpResponseMessage response = await response_contents;
-                crawllogger.AddLog(target);
-                taskCount--;
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine("Max Threads " + crawler_engine.MaxConcurrency);
+                Console.WriteLine("Last Crawled " + lastCrawled);
+                Console.WriteLine("Tasks Queued " + crawler_engine.TaskCount);
+                await Task.Delay(TimeSpan.FromMilliseconds(100));
+                Console.WriteLine("-----------------------------------------------------------" + "\n\n");
 
-                try
+                while (crawler_engine.MessageQueue.Count > 0)
                 {
-                    if (response.IsSuccessStatusCode)
-                    {
-                        // by calling .Result you are performing a synchronous call
-                        var responseContent = response.Content;
-
-                        // by calling .Result you are synchronously reading the result
-                        responseString = responseContent.ReadAsStringAsync().Result;
-
-                    }
-                }
-                catch (Exception ex)
-                {
-                    responseString = "";
-                }
-            }
-            catch (ArgumentException)
-            {
-                Console.WriteLine("An element with Key = {0} already exists.", target);
-                responseString = "";
-            }
-           
-       
-           
-
-            getLinks(responseString, target);
-            return responseString;
-           
-        }
-
-        static void getLinks(string body, string target)
-        {
-            MatchCollection matches = re.Matches(body);
-
-            if (matches.Count > 0)
-            {
-                foreach (Match match in matches)
-                {
-                    var test = matches;
-                    string current_match = "";
-                    current_match = match.Groups[1].Value;
-
-                    string newTarget = target + current_match;
-                    //Console.WriteLine(match.Groups);
-
-
-
-                   // Internal site match
-                    if (current_match.FirstOrDefault() == '/')
-                    {
-                        newTarget = "http://" + slash_re.Replace((target + current_match).Replace("http://", ""), "/");
-
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        //Console.WriteLine(newTarget);
-                        Console.ResetColor();
-
-                        if (!history.ContainsKey(newTarget))
-                        {
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            //Console.WriteLine("NEW: " + (newTarget));
-                            Console.ResetColor();
-                            _urls.Push(newTarget);
-                        }
-                        else if (history.ContainsKey(newTarget))
-                        {
-                            Console.ForegroundColor = ConsoleColor.Yellow;
-                            //Console.WriteLine("EXISTS: " + (newTarget));
-                            Console.ResetColor();
-                        }
-
-                    }
-
-                    //external site
-
-                    if (current_match.IndexOf("http") == 0 || current_match.IndexOf("https") == 0)
-                    {
-                        newTarget = current_match;
-                        if (!history.ContainsKey(newTarget))
-                        {
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            //Console.WriteLine("NEW EXTERNAL: " + (newTarget));
-                            Console.ResetColor();
-                            _urls.Push(newTarget);
-                        }
-                        else if (history.ContainsKey(newTarget))
-                        {
-                            Console.ForegroundColor = ConsoleColor.Yellow;
-                            //Console.WriteLine("EXISTING EXTERNAL: " + (newTarget));
-                            Console.ResetColor();
-                        }
-                    }
+                    var message = crawler_engine.MessageQueue.Pop();
+                    Console.WriteLine(message);
                 }
             }
         }
     }
 }
-
-
-
